5662b1a6c9c9ae0ddf58bbbd0b93b48376cb283c
ofmartinezm <oscar.masmela@gmail.com>
2017-10-24 13:48:35 -0500

    Se adicionan comentarios en cada url

--- recetarioII/principal/urls.py
+++ recetarioII/principal/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
 	#url(r'^lista/',views.lista_bebidas),
 	url(r'^$', lista_bebidas.as_view(), name='lista'),
 	url(r'^nuevo$', BebidaCreation.as_view(), name='nuevo'),
+	# con el formato se indica que se va a enviar un dato numerico y de tipo primary key
 	url(r'^(?P<pk>\d+)$', BebidaDetalle.as_view(), name='detalle'),
 	#url(r'^detalle/(?P<nombre>[-\w\s]+)$', BebidaDetalle.as_view(), name='detalle'),
-	
+	# con el formato (?P<nombre>[-\w\s]+)$ se indica que se va a enviar el parametro nombre de tipo string que admite espacios en blanco
+
 
 ]

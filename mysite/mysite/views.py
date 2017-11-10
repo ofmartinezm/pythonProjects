cb4e365542501e2e3415ff55c9f6827e73c3c062
ofmartinezm <oscar.masmela@gmail.com>
2017-11-10 17:11:24 -0500

    se actualiza el proyecto se adiciono manejo de plantillas

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 
 from django.template.loader import get_template
 from django.template import Context
@@ -11,11 +11,11 @@ def current_datetime(request):
     #t=get_template('current_datetime.html')
     #t = render_to_string('current_datetime.html', ctx)
     #html="<html><body>It is now %s.</body></html>" % now
-    return render(request, 'current_datetime.html',{'current_datetime':now})
-
+    #return render(request, 'current_datetime.html',{'current_datetime':now})
+    return render_to_response('current_datetime.html', {'current_date': now})
 
 def hours_ahead(request, offset):
     #offset = int(offset)
-    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    dt = datetime.datetime.now() + datetime.timedelta(hours=int(offset))
+    #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+    return render_to_response('hours_ahead.html', {'next_time': dt})
